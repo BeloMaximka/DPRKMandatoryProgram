ad6333b29921768a2b4df419dc6195317effd9c8
BeloMaximka <belomaxim@gmail.com>
2022-09-09 10:18:16 +0300

    Добавьте файлы проекта.

---
+++ DPRKMandatoryProgram/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace DPRKMandatoryProgram
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startRecordingButton = new System.Windows.Forms.Button();
+            this.screenshotTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // startRecordingButton
+            // 
+            this.startRecordingButton.Location = new System.Drawing.Point(8, 8);
+            this.startRecordingButton.Name = "startRecordingButton";
+            this.startRecordingButton.Size = new System.Drawing.Size(96, 23);
+            this.startRecordingButton.TabIndex = 0;
+            this.startRecordingButton.Text = "Начать слежку";
+            this.startRecordingButton.UseVisualStyleBackColor = true;
+            this.startRecordingButton.Click += new System.EventHandler(this.startRecordingButton_Click);
+            // 
+            // screenshotTimer
+            // 
+            this.screenshotTimer.Interval = 5000;
+            this.screenshotTimer.Tick += new System.EventHandler(this.screenshotTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(142, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество скриншотов: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(202, 62);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startRecordingButton);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Весёлый Шпионаж";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startRecordingButton;
+        private System.Windows.Forms.Timer screenshotTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ DPRKMandatoryProgram/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace DPRKMandatoryProgram
+{
+    public partial class Form1 : Form
+    {
+
+        string path;
+        int screenshotCounter = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startRecordingButton_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+            if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = folderBrowserDialog.SelectedPath;
+                button.Text = "Слежка начата.";
+                screenshotTimer.Enabled = true;
+            }
+        }
+
+        private void screenshotTimer_Tick(object sender, EventArgs e)
+        {
+            Bitmap bmpScreenshot;
+            Graphics gfxScreenshot;
+            int screenCount = 1;
+            foreach (Screen screen in Screen.AllScreens)
+            {
+                bmpScreenshot = new Bitmap(screen.Bounds.Width, screen.Bounds.Height, PixelFormat.Format32bppArgb);
+                gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+                gfxScreenshot.CopyFromScreen(screen.Bounds.X, screen.Bounds.Y, 0, 0, screen.Bounds.Size, CopyPixelOperation.SourceCopy);
+                bmpScreenshot.Save($"{path}\\{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}-{screenCount}.png", ImageFormat.Png);
+                screenCount++;
+                screenshotCounter++;
+            }
+
+            label1.Text = $"Количество скриншотов: {screenshotCounter}";
+        }
+    }
+}
